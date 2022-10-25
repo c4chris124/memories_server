455f1a69fbd233614146eed4014ff2963e983419
Christian Castellanos <c4chris124@gmail.com>
2022-10-24 19:35:12 -0600

    imported mongo model for post, added logic to get and create post'

--- controllers/posts.js
+++ controllers/posts.js
@@ -1,3 +1,27 @@
-export const getPosts = (req, res) => {
-    res.send('This works')
+import PostMessage from "../models/postMessage.js"
+
+// get post func
+export const getPosts = async (req, res) => {
+    try {
+        // query to find post through mondodb asyc
+        const postMessages = await PostMessage.find()
+        res.status(200).json(postMessages)
+    } catch (error) {
+        res.status(404).json({message: error.message})
+    }
+}
+// Adding new post, create func
+export const createPost = async (req, res) => {
+    // body of request
+    const post = req.body
+
+    // new post creation, passing post request body info
+    const newPost = new PostMessage(post)
+
+    try {
+        await newPost.save()
+        res.status(201).json(newPost)
+    } catch (error) {
+        res.status(409).json({message: error.message})
+    }
 }
